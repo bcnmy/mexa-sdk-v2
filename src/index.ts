a35f0c40e26e1e3a77b2e5df5e05c517f8841811
arcticfloyd1984 <yashasvichaudhry98@gmail.com>
2022-04-19 14:58:14 +0530

    basic setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @dev Biconomy class that is the entry point
+ */
+import EventEmitter from 'events';
+
+export class Biconomy extends EventEmitter {
+  constructor(provider: any, options) {
+    // check and validate options
+    _init();
+  }
+
+  _init() {
+    // intersect JSON RPC Call
+  }
+}
