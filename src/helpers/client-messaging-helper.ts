fd2e79520b885baf0b36c1feab501aca8623fbd0
arcticfloyd1984 <yashasvichaudhry98@gmail.com>
2022-07-10 23:49:29 +0530

    in middle of mess

--- src/helpers/client-messaging-helper.ts
+++ src/helpers/client-messaging-helper.ts
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-await-in-loop */
 import { ClientMessenger } from 'gasless-messaging-sdk';
 import { logMessage } from '../utils';
 import type { Biconomy } from '..';
@@ -7,6 +9,19 @@ const clientMessenger = new ClientMessenger(
   config.webSocketConnectionUrl,
 );
 
+const retryConnect = async () => {
+  if (!clientMessenger.socketClient.isConnected()) {
+    console.log('connecting clientMessenger');
+    try {
+      await clientMessenger.connect();
+      console.log('returning true');
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+};
 export const mexaSdkClientMessenger = async (
   engine: Biconomy,
   transactionData: { transactionId: string },
@@ -14,49 +29,55 @@ export const mexaSdkClientMessenger = async (
   try {
     const { transactionId } = transactionData;
 
-    console.log('Emitting test event');
-    engine.emit('test', {
-      test: 'test',
-    });
-
-    console.log('Test event emitted');
     console.log(clientMessenger.socketClient.isConnected());
+
     if (!clientMessenger.socketClient.isConnected()) {
-      console.log('connecting clientMessenger');
-      await clientMessenger.connect();
-      console.log('clientMessenger is connected');
+      let isConnectionMade = await retryConnect();
+      const connectionCount = 1;
+      for (let retryIndex = 0; retryIndex < 10; retryIndex += 1) {
+        if (!isConnectionMade) {
+          console.log('connectionCount', connectionCount);
+          isConnectionMade = await retryConnect();
+          console.log('isConnectionMade', isConnectionMade);
+        } else {
+          break;
+        }
+      }
+      console.log('connection made');
     }
+    console.log('connection made outside while');
+    console.log(clientMessenger.socketClient.isConnected());
 
-    // clientMessenger.createTransactionNotifier(transactionId, {
-    //   onMined: (tx: { transactionId: string; transactionHash: string; }) => {
-    //     console.log(`Tx Hash mined message received at client with id ${tx.transactionId} and hash ${tx.transactionHash}`);
+    clientMessenger.createTransactionNotifier(transactionId, {
+      onMined: (tx: { transactionId: string; transactionHash: string; }) => {
+        console.log(`Tx Hash mined message received at client with id ${tx.transactionId} and hash ${tx.transactionHash}`);
 
-    //     engine.emit('txMined', () => ({
-    //       msg: 'txn mined',
-    //       id: tx.transactionId,
-    //       hash: tx.transactionHash,
-    //     }));
-    //   },
-    //   onHashGenerated: (tx: { transactionId: string; transactionHash: string; }) => {
-    //     console.log(`Tx Hash generated message received at client ${tx.transactionId} and hash ${tx.transactionHash}`);
+        engine.emit('txMined', () => ({
+          msg: 'txn mined',
+          id: tx.transactionId,
+          hash: tx.transactionHash,
+        }));
+      },
+      onHashGenerated: (tx: { transactionId: string; transactionHash: string; }) => {
+        console.log(`Tx Hash generated message received at client ${tx.transactionId} and hash ${tx.transactionHash}`);
 
-    //     engine.emit('txHashGenerated', () => ({
-    //       msg: 'hash generated',
-    //       id: tx.transactionId,
-    //       hash: tx.transactionHash,
-    //     }));
-    //   },
-    //   // TODO
-    //   // Change type in messaging sdk
-    //   onError: (errorResponseData: { error: any; transactionId: string; }) => {
-    //     console.log(`Error message received at client\n ${errorResponseData.error}`);
-    //     engine.emit('onError', () => ({
-    //       // code: errorResponseData.code,
-    //       error: errorResponseData.error,
-    //       transactionId: errorResponseData.transactionId,
-    //     }));
-    //   },
-    // });
+        engine.emit('txHashGenerated', () => ({
+          msg: 'hash generated',
+          id: tx.transactionId,
+          hash: tx.transactionHash,
+        }));
+      },
+      // TODO
+      // Change type in messaging sdk
+      onError: (errorResponseData: { error: any; transactionId: string; }) => {
+        console.log(`Error message received at client\n ${errorResponseData.error}`);
+        engine.emit('onError', () => ({
+          // code: errorResponseData.code,
+          error: errorResponseData.error,
+          transactionId: errorResponseData.transactionId,
+        }));
+      },
+    });
   } catch (error) {
     console.log(error);
     logMessage(JSON.stringify(error));
