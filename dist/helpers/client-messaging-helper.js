c4f50e2960204644d2ebdb524266ae57d11c2ef8
Kunal Chauhan <chauhan.kunal4@gmail.com>
2022-07-11 18:42:17 +0530

    added dist folder

---
+++ dist/helpers/client-messaging-helper.js
@@ -0,0 +1,50 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mexaSdkClientMessenger = void 0;
+/* eslint-disable consistent-return */
+const utils_1 = require("../utils");
+const mexaSdkClientMessenger = (engine, transactionData) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { transactionId } = transactionData;
+        engine.clientMessenger.createTransactionNotifier(transactionId, {
+            onMined: (tx) => {
+                (0, utils_1.logMessage)(`Tx Hash mined message received at client with id ${tx.transactionId} and hash ${tx.transactionHash}`);
+                engine.emit('txMined', {
+                    msg: 'txn mined',
+                    id: tx.transactionId,
+                    hash: tx.transactionHash,
+                });
+            },
+            onHashGenerated: (tx) => {
+                (0, utils_1.logMessage)(`Tx Hash generated message received at client ${tx.transactionId} and hash ${tx.transactionHash}`);
+                engine.emit('txHashGenerated', {
+                    msg: 'hash generated',
+                    id: tx.transactionId,
+                    hash: tx.transactionHash,
+                });
+            },
+            onError: (errorResponseData) => {
+                (0, utils_1.logMessage)(`Error message received at client\n ${errorResponseData.error}`);
+                engine.emit('onError', {
+                    error: errorResponseData.error,
+                    transactionId: errorResponseData.transactionId,
+                });
+            },
+        });
+    }
+    catch (error) {
+        (0, utils_1.logMessage)(error);
+        (0, utils_1.logMessage)(JSON.stringify(error));
+    }
+});
+exports.mexaSdkClientMessenger = mexaSdkClientMessenger;
+//# sourceMappingURL=client-messaging-helper.js.map
